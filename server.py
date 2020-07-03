952cbffb788cecd053572d5d488a7870702a56cb
ehtesham1999 <ehteshamhussain1999@gmail.com>
2020-07-03 20:23:17 +0530

    1st committ

---
+++ server.py
@@ -0,0 +1,27 @@
+from flask import Flask, request, jsonify, render_template
+import util
+
+app = Flask(__name__)
+
+@app.route('/',methods=['GET'])
+def Home():
+    locations = util.get_location_names()
+    return render_template('index.html',locationss=locations)
+
+
+@app.route('/predict_home_price', methods=['POST'])
+def predict_home_price():
+    total_sqft = float(request.form['total_sqft'])
+    location = request.form['location']
+    bhk = int(request.form['bhk'])
+    bath = int(request.form['bath'])
+
+    output =  util.get_estimated_price(location,total_sqft,bhk,bath)
+
+    return render_template('index.html', prediction_text="You Can Sell The House at {}".format(output))
+
+
+if __name__ == "__main__":
+    print("Starting Python Flask Server For Home Price Prediction...")
+    util.load_saved_artifacts()
+    app.run()
